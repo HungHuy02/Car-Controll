daf11bf7277339ad71a81658ac6780baee32f32d
HungHuy02 <nguyenhunghuy.1234@gmail.com>
2024-07-06 20:45:42 +0700

    code optimization

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -14,14 +14,16 @@
 #define DR_S 13
 #define UL_S A0
 #define UR_S A1
-#define echo A2
-#define trig A3
-#define servoPin 7
+#define ECHO A2
+#define TRIG A3
+#define Servo_PIN 7
 #define MAX_DISTANCE 200
-#define gocTruoc 72
+#define FRONT_ANGLE 72
+#define CONTROL_SPEED 150
+#define AUTO_SPEED 100
 
 SoftwareSerial bluetooth(9, 8);
-NewPing sonar(trig, echo, MAX_DISTANCE);
+NewPing sonar(TRIG, ECHO, MAX_DISTANCE);
 Servo myServo;
 
 TaskHandle_t xTaskHandleConnect;
@@ -34,19 +36,12 @@ TaskHandle_t xTaskAutoObstacle = NULL;
 QueueHandle_t queueCommand;
 QueueHandle_t queueJson;
 
-byte Speed = 100;
-
 byte run = 0;
 
 bool wifiOn = true;
 bool bluetoothOn = true;
 bool isHandle = false;
 bool isAuto = false;
-
-void forward();
-void back();
-void left();
-void right();
 
 void setup() {
   pinMode(IN3, OUTPUT);
@@ -57,7 +52,7 @@ void setup() {
   pinMode(DL_S, INPUT);
   pinMode(UR_S, INPUT);
   pinMode(UL_S, INPUT);
-  myServo.attach(servoPin);
+  myServo.attach(Servo_PIN);
   bluetooth.begin(9600);
   Wire.begin(100);
   Wire.onReceive(receiveEvent);
@@ -125,7 +120,6 @@ void vHandleData(void* pvParameters) {
     switch (command) {
       case 'Y':
         myServo.detach();
-        Speed = 100;
         temp = 'Y';
         if (!isAuto) {
           isAuto = true;
@@ -133,24 +127,22 @@ void vHandleData(void* pvParameters) {
         }
         break;
       case 'Z':
-        myServo.attach(servoPin);
-        Speed = 100;
+        myServo.attach(Servo_PIN);
         temp = 'Z';
         if (!isAuto) {
           isAuto = true;
           xTaskCreate(vAutoObstacle, "Task5", 198, NULL, configMAX_PRIORITIES - 1, &xTaskAutoObstacle);
         }
-        myServo.write(gocTruoc);
+        myServo.write(FRONT_ANGLE);
         break;
       case 'T':
-        myServo.attach(servoPin);
-        Speed = 100;
+        myServo.attach(Servo_PIN);
         temp = 'T';
         if (!isAuto) {
           isAuto = true;
           xTaskCreate(vAutoFollow, "Task6", 198, NULL, configMAX_PRIORITIES - 1, &xTaskAutoFollow);
         }
-        myServo.write(gocTruoc);
+        myServo.write(FRONT_ANGLE);
         break;
       case 'D':
         bluetoothOn = false;
@@ -177,23 +169,19 @@ void vHandleControl(void* pvParameters) {
 
     switch (command) {
       case 'F':
-        Speed = 150;
-        forward();
+        forward(CONTROL_SPEED);
         temp = 'F';
         break;
       case 'B':
-        Speed = 150;
-        back();
+        back(CONTROL_SPEED);
         temp = 'B';
         break;
       case 'L':
-        Speed = 150;
-        left();
+        left(CONTROL_SPEED);
         temp = 'L';
         break;
       case 'R':
-        Speed = 150;
-        right();
+        right(CONTROL_SPEED);
         temp = 'R';
         break;
       case 'S':
@@ -215,19 +203,19 @@ void vHandleControl(void* pvParameters) {
 
 
 void vAutoLine(void* pvParameters) {
-  int Dleft_sensor;
-  int Dright_sensor;
+  byte Dleft_sensor;
+  byte Dright_sensor;
   for (;;) {
     Dleft_sensor = digitalRead(DL_S);
     Dright_sensor = digitalRead(DR_S);
     if (Dleft_sensor == 0 && Dright_sensor == 1) {
       if (run != 3) {
         Stop();
-        back();
+        back(AUTO_SPEED);
         vTaskDelay(100 / portTICK_PERIOD_MS);
         Stop();
       }
-      right();
+      right(AUTO_SPEED);
       if (wifiOn) {
         String json = "{\"Dl\":" + String(Dleft_sensor) + ",\"Dr\":" + String(Dright_sensor) + "}";
         xQueueSendToBack(queueJson, &json, portMAX_DELAY);
@@ -236,11 +224,11 @@ void vAutoLine(void* pvParameters) {
     } else if (Dleft_sensor == 1 && Dright_sensor == 0) {
       if (run != 4) {
         Stop();
-        back();
+        back(AUTO_SPEED);
         vTaskDelay(100 / portTICK_PERIOD_MS);
         Stop();
       }
-      left();
+      left(AUTO_SPEED);
       if (wifiOn) {
         String json = "{\"Dl\":" + String(Dleft_sensor) + ",\"Dr\":" + String(Dright_sensor) + "}";
         xQueueSendToBack(queueJson, &json, portMAX_DELAY);
@@ -251,57 +239,83 @@ void vAutoLine(void* pvParameters) {
         Stop();
         vTaskDelay(100 / portTICK_PERIOD_MS);
       }
-      forward();
+      forward(AUTO_SPEED);
     }
   }
 }
 
 void vAutoObstacle() {
-  int distance = 100;
-  int distanceL = 100;
-  int distanceR = 100;
-  distance = readPing();
-  if (distance <= 15) {
-    Stop();
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    back();
-    vTaskDelay(300 / portTICK_PERIOD_MS);
-    Stop();
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    distanceR = lookR();
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    distanceL = lookL();
-    if (wifiOn) {
-      String json = "{\"d\":" + String(distance) + ",\"dR\":" + String(distanceR) + ",\"dL\":" + String(distanceL) + "}";
-      xQueueSendToBack(queueJson, &json, portMAX_DELAY);
-    }
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    if (distanceR >= distanceL) {
+  byte distance = 100;
+  byte distanceL = 100;
+  byte distanceR = 100;
+  for (;;) {
+    distance = readPing();
+    if (distance <= 15) {
       Stop();
-      right();
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+      back(AUTO_SPEED);
+      vTaskDelay(300 / portTICK_PERIOD_MS);
       Stop();
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+      distanceR = lookR();
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+      distanceL = lookL();
+      if (wifiOn) {
+        String json = "{\"d\":" + String(distance) + ",\"dR\":" + String(distanceR) + ",\"dL\":" + String(distanceL) + "}";
+        xQueueSendToBack(queueJson, &json, portMAX_DELAY);
+      }
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+      if (distanceR >= distanceL) {
+        Stop();
+        right(AUTO_SPEED);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        Stop();
+      } else {
+        Stop();
+        left(AUTO_SPEED);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        Stop();
+      }
     } else {
-      Stop();
-      left();
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-      Stop();
+      forward(AUTO_SPEED);
     }
-  } else {
-    forward();
   }
 }
 
 void vAutoFollow() {
-  int distance = 100;
-  int Uleft_sensor;
-  int Uright_sensor;
-  distance = readPing();
-  Uright_sensor = digitalRead(UR_S);
-  Uleft_sensor = digitalRead(UL_S);
-  if (Uleft_sensor != 0 && Uright_sensor != 0) {
-    if (distance <= 8) {
-      if (run != 2) {
+  byte distance = 100;
+  byte Uleft_sensor;
+  byte Uright_sensor;
+  for (;;) {
+    distance = readPing();
+    Uright_sensor = digitalRead(UR_S);
+    Uleft_sensor = digitalRead(UL_S);
+    if (Uleft_sensor != 0 && Uright_sensor != 0) {
+      if (distance <= 8) {
+        if (run != 2) {
+          Stop();
+          vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        if (wifiOn) {
+          String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
+          xQueueSendToBack(queueJson, &json, portMAX_DELAY);
+        }
+        back(AUTO_SPEED);
+      } else if (distance <= 25 && distance > 12) {
+        if (run != 1) {
+          Stop();
+          vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        if (wifiOn) {
+          String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
+          xQueueSendToBack(queueJson, &json, portMAX_DELAY);
+        }
+        forward(AUTO_SPEED);
+      } else {
+        Stop();
+      }
+    } else if (Uleft_sensor == 0 && Uright_sensor != 0) {
+      if (run != 3) {
         Stop();
         vTaskDelay(100 / portTICK_PERIOD_MS);
       }
@@ -309,9 +323,9 @@ void vAutoFollow() {
         String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
         xQueueSendToBack(queueJson, &json, portMAX_DELAY);
       }
-      back();
-    } else if (distance <= 25 && distance > 12) {
-      if (run != 1) {
+      right(AUTO_SPEED);
+    } else if (Uleft_sensor != 0 && Uright_sensor == 0) {
+      if (run != 4) {
         Stop();
         vTaskDelay(100 / portTICK_PERIOD_MS);
       }
@@ -319,82 +333,60 @@ void vAutoFollow() {
         String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
         xQueueSendToBack(queueJson, &json, portMAX_DELAY);
       }
-      forward();
-    } else {
+      left(AUTO_SPEED);
+    } else if (Uleft_sensor == 0 && Uright_sensor == 0) {
       Stop();
     }
-  } else if (Uleft_sensor == 0 && Uright_sensor != 0) {
-    if (run != 3) {
-      Stop();
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    if (wifiOn) {
-      String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
-      xQueueSendToBack(queueJson, &json, portMAX_DELAY);
-    }
-    right();
-  } else if (Uleft_sensor != 0 && Uright_sensor == 0) {
-    if (run != 4) {
-      Stop();
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    if (wifiOn) {
-      String json = "{\"d\":" + String(distance) + ",\"Ur\":" + String(Uright_sensor) + ",\"Ul\":" + String(Uleft_sensor) + "}";
-      xQueueSendToBack(queueJson, &json, portMAX_DELAY);
-    }
-    left();
-  } else if (Uleft_sensor == 0 && Uright_sensor == 0) {
-    Stop();
   }
 }
 
-int readPing() {
+byte readPing() {
   vTaskDelay(70 / portTICK_PERIOD_MS);
-  int cm = sonar.ping_cm();
+  byte cm = sonar.ping_cm();
   if (cm == 0) {
     cm = 250;
   }
   return cm;
 }
 
-int lookR() {
-  myServo.write(gocTruoc - 50);
+byte lookR() {
+  myServo.write(FRONT_ANGLE - 50);
   vTaskDelay(250 / portTICK_PERIOD_MS);
-  int distance = readPing();
+  byte distance = readPing();
   vTaskDelay(200 / portTICK_PERIOD_MS);
-  myServo.write(gocTruoc);
+  myServo.write(FRONT_ANGLE);
   return distance;
 }
 
-int lookL() {
-  myServo.write(gocTruoc + 50);
+byte lookL() {
+  myServo.write(FRONT_ANGLE + 50);
   vTaskDelay(250 / portTICK_PERIOD_MS);
-  int distance = readPing();
+  byte distance = readPing();
   vTaskDelay(200 / portTICK_PERIOD_MS);
-  myServo.write(gocTruoc);
+  myServo.write(FRONT_ANGLE);
   return distance;
 }
 
-void left() {
+void left(byte speed) {
   run = 4;
-  analogWrite(IN1, Speed);
-  analogWrite(IN3, Speed);
+  analogWrite(IN1, speed);
+  analogWrite(IN3, speed);
 }
-void right() {
+void right(byte speed) {
   run = 3;
-  analogWrite(IN2, Speed);
-  analogWrite(IN4, Speed);
+  analogWrite(IN2, speed);
+  analogWrite(IN4, speed);
 }
-void back() {
+void back(byte speed) {
   run = 2;
-  analogWrite(IN3, Speed);
-  analogWrite(IN2, Speed);
+  analogWrite(IN3, speed);
+  analogWrite(IN2, speed);
 }
 
-void forward() {
+void forward(byte speed) {
   run = 1;
-  analogWrite(IN4, Speed);
-  analogWrite(IN1, Speed);
+  analogWrite(IN4, speed);
+  analogWrite(IN1, speed);
 }
 void Stop() {
   run = 0;
